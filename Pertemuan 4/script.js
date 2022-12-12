65a8a84f5ed0c1d83d84b591a32200294f2ee5dc
Nadhif-92 <103347734+Nadhif-92@users.noreply.github.com>
2022-12-12 23:11:04 +0700

    Add files via upload

---
+++ Pertemuan 4/script.js
@@ -0,0 +1,9 @@
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+    document.body.classList.toggle("lightmode");
+    if(document.body.classList.contains("lightmode")){
+        icon.src = "img/lightmode.png";
+    } else {
+        icon.src = "img/darkmode.png";
+    }
+}
